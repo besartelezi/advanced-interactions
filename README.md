4038adbfc067b49cc5d8f1464bf8f39d666ab5c8
Besart Elezi <besart.elezi@hotmail.com>
2022-05-30 14:20:54 +0200

    added carousel
    It's still not parallax and has a ton of bugs

--- README.md
+++ README.md
@@ -10,12 +10,23 @@ Advanced Interactions with Javascript, now that's a challenge! The requirements 
 * Have letters that are inside a span class do something of your choosing (let the letter "e" disappear, let the letter "a" rotate, etc.).
 * A lightmode/darkmode switch
 
-The minimum requirement for this project are just the three first requirements and one chasing circle of your choosing. I will however, try to finish all requirements. If I run out of time before I can finish everything, I want to at least have a funcioning lightmode/darkmode switch. I think learning how to make such a switch might be very essential to becoming a pro Web Developer. Also I hate lightmode. 
+The minimum requirement for this project are just the three first requirements and one chasing circle of your choosing. I will however, try to finish all requirements. If I run out of time before I can finish everything, I want to at least have a functioning lightmode/darkmode switch. I think learning how to make such a switch might be very essential to becoming a pro Web Developer. Also, I hate lightmode. 
 
 ## Parallax Carousel, not Paarthurnax Carousel
 I'd like to say that I know how to start with this one, but that would make me a dirty liar. For now, I will try and figure out how I'd like to start on creating a parallax carousel, and look for how other people have made their parallax carousels. </br>
 
 The way I'm thinking of doing it, is by creating an array of objects, with the objects being different images. Then, have a 'back' and 'next' button at the sides of the carousel, that will just change the image src of the carousel to the next one in the array. But what about the parallax effect? <br>
 
+I'll add a parallax effect to my images once I'm done with the carousel. I will admit though, that I find this part of the project to be really overwhelming. But it does make me appreciate Bootstrap a **lot** more. Figuring out how to start on this project is a challenge in and of itself.
+
 As of now, I might need to take a step back and take in all kinds of new information. If I start on this project by just using my current knowledge of Javascript, I'll learn nothing. So the very first step will be research, *research*, and **research**!
+
+## Fun Title Name
+For now, I have decided on creating a carousel first and then adding the parallax effect on the images afterwards. I won't be using my previous idea of an array. I will try out the method of creating a carousel that is mentioned on [this website!](https://engineertodeveloper.com/create-a-carousel-with-vanilla-javascript/) <br>
+
+The website mentioned that: *"For the initial carousel styles, we will position the carousel items relative to the carousel container; this will allow the carousel items to stack on top of each other, like a stack of papers."* </br>I have never even thought of creating the carousel like this. So hurray for research! <br>
+
+I have also chosen to follow the steps on that website, because they're using Javascript classes, something I'm **extremely** unfamiliar with, but something I really want to learn. 
+
+## The Ups and Downs of the Learning Experience
 
--- script.js
+++ script.js
@@ -6,3 +6,93 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+class Slider {
+    constructor(sliderElement) {
+        this.slider = sliderElement;
+        this.sliderItems = sliderElement.getElementsByClassName("SliderItem");
+        this.nextButton = sliderElement.querySelector(".SliderControlNext");
+        this.previousButton = sliderElement.querySelector(".SliderControlPrevious");
+        this.currentIndex = 0;
+        this.previousItemIndex = this.sliderItems.length -1;
+        this.nextItemIndex = 1;
+        this.isSliding = false;
+        this.setEventListeners();
+    }
+    //Setting event listeners, when they click on next, the Next function will be activated, when clicking on Previous, the Previous function will be activated
+    setEventListeners(){
+        this.previousButton.addEventListener("click", () => {
+            this.Previous();
+        });
+        this.nextButton.addEventListener("click", () => {
+            this.Next()
+        })
+    }
+
+    setIndices(direction) {
+        let index;
+        if (direction === "NEXT") {
+            index = this.currentIndex === this.sliderItems.length - 1 ? 0 : this.currentIndex +1;
+        }
+        if (direction ==="PREVIOUS"){
+            index = this.currentIndex === 0 ? this.sliderItems.length - 1: this.currentIndex -1;
+        }
+        if (index === 0) {
+            this.currentIndex = index;
+            this.nextItemIndex = index + 1;
+            this.previousItemIndex = this.sliderItems.length - 1;
+        }
+        else if (index === this.sliderItems.length -1) {
+            this.currentIndex = this.sliderItems.length - 1;
+            this.nextItemIndex = 0;
+            this.previousItemIndex = this.currentIndex -1;
+        }
+        else {
+            this.currentIndex = index;
+            this.nextItemIndex = index + 1;
+            this.previousItemIndex = index - 1;
+        }
+    }
+
+    //The main way that animations and actions are used here, is by adding a string to the current classname. So when you click on a button, it changes the classname of an element, then, in the CSS, that new classname has a separate CSS.
+    //This is a way that I haven't used Javascript before, but it really is informative
+    Next() {
+        //If the slider is currently busy sliding to the next button, the user won't be able to press the button
+        if (this.isSliding) return;
+        this.isSliding = true;
+        this.sliderItems[this.nextItemIndex].classList.add("NextItem");
+        //this adds a new text to the current classname of the elements, so the current CSS code doesn't apply to it. However, once the classname has been changed, a new CSS styling will be applied to it.
+        setTimeout(()=> {
+            this.sliderItems[this.currentIndex].classList.add("SlideNext");
+            this.sliderItems[this.nextItemIndex].classList.add("SlideEnd");
+            this.sliderItems[this.nextItemIndex].classList.add("Active");
+        }, 20);
+
+        setTimeout(()=> {
+            this.sliderItems[this.nextItemIndex].classList.remove("NextItem", "SlideEnd");
+            this.sliderItems[this.currentIndex].classList.remove("SlideNext", "Active");
+            this.setIndices("NEXT");
+            this.isSliding = false;
+        }, 400);
+    }
+    Previous() {
+        if (this.isSliding) return;
+        this.isSliding = true;
+        this.sliderItems[this.previousItemIndex].classList.add("PreviousItem");
+        setTimeout(() => {
+            this.sliderItems[this.currentIndex].classList.add("SlidePrevious");
+            this.sliderItems[this.previousItemIndex].classList.add("SlideEnd");
+            this.sliderItems[this.previousItemIndex].classList.add("Active");
+        }, 20)
+        setTimeout(() => {
+            this.sliderItems[this.previousItemIndex].classList.remove("PreviousItem", "SlideEnd");
+            this.sliderItems[this.currentIndex].classList.remove("SlidePrevious", "Active");
+            this.isSliding = false;
+            this.setIndices("PREV");
+        },400);
+    }
+}
+
+const slider = new Slider(
+    document.querySelector(".CarouselSlider")
+);
